38aacb8627f855598195bad959220987fe24f637
Ivan <Ivan@KOMPUTER>
2022-10-22 00:59:41 +0700

    Create client project

---
+++ DotaCounterPicker.Client/DotaClientExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace DotaCounterPicker.Client;
+
+public partial class DotaClient
+{
+    public Task PrepareRequestAsync(HttpClient? client, HttpRequestMessage? message, StringBuilder? url, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task PrepareRequestAsync(HttpClient? client, HttpRequestMessage? message, string url, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task ProcessResponseAsync(HttpClient? client, HttpResponseMessage? message, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
