f68a5b1a93493ef07714f0d99b97b04cae410708
Ivan <Ivan@KOMPUTER>
2022-11-05 14:21:34 +0700

    add hero images in result table

--- DotaCounterPicker.Server/Utils/GlobalConsts.cs
+++ DotaCounterPicker.Server/Utils/GlobalConsts.cs
@@ -5,4 +5,6 @@ public static class GlobalConsts
     public static string EmptyHero = "empty";
 
     public static string HeroImageExtension = ".jpg";
+
+    public static string ImagePath = $"{Directory.GetCurrentDirectory()}{@"\wwwroot\heroes"}";
 }
