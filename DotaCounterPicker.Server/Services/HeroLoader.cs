739a2dd1d437b1270ff675b4946e44b22d2d4947
Ivan <Ivan@KOMPUTER>
2022-12-10 23:13:30 +0700

    used HttpClient from ctor in HeroLoader

--- DotaCounterPicker.Server/Services/HeroLoader.cs
+++ DotaCounterPicker.Server/Services/HeroLoader.cs
@@ -14,10 +14,9 @@ public class HeroLoader : IHeroLoader
     public async Task<string> LoadHero(string heroName)
     {
         var url = $"https://www.dotabuff.com/heroes/{heroName}/counters";
-        HttpClient httpClient = new HttpClient();
         var request = new HttpRequestMessage(HttpMethod.Get, url);
         request.Headers.UserAgent.ParseAdd("Mozilla/5.0 (compatible; AcmeInc/1.0)");
-        var res = await httpClient.SendAsync(request);
+        var res = await _client.SendAsync(request);
         return await res.Content.ReadAsStringAsync();
     }
 }
